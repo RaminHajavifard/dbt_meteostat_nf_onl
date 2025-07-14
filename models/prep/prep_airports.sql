604f73b5a5be10e2b9f936653e2a23eaa2f2d27d
RaminHajavifard <raminhajavifard@yahoo.com>
2025-07-14 13:49:13 +0200

    edit

--- models/prep/prep_airports.sql
+++ models/prep/prep_airports.sql
@@ -1,14 +1,15 @@
 WITH airports_reorder AS (
     SELECT faa
-    	   ,country
-    	   ,region
-           ,name
-           ,city
-           ,lat
-           ,lon
-           ,alt
-           ,tz
-           ,dst
-    FROM {{ref('staging_airports')}}
+         , name
+         , country
+         , region        
+         , city
+         , lat
+         , lon
+         , alt
+         , tz
+         , dst
+    FROM {{ ref('staging_airports') }}
 )
-SELECT * FROM airports_reorder
+SELECT *
+FROM airports_reorder
